4419e5e4ed9b57e109182c67cea23b1a75661ae7
ed <s@ocv.me>
2025-10-04 09:35:31 +0000

    Update util.js
    Signed-off-by: ed <s@ocv.me>

--- copyparty/web/util.js
+++ copyparty/web/util.js
@@ -1040,10 +1040,8 @@ function humansize_fuzzy(b) {
 	if (b <= 1300000000) return "GD-ROM";
 	if (b <= 4700000000) return "DVD";
 	if (b <= 9400000000) return "DVD-DL";
-	if (b <= 25000000000) return "BD";
-	if (b <= 50000000000) return "BD-DL";
-	if (b <= 100000000000) return "BDXL-TL";
-	if (b <= 128000000000) return "BDXL-QL";
+	if (b <= 25025000000) return "BluRei";
+	if (b <= 50050000000) return "BD-DL";
 	return "LTO";
 }
 var humansize_fmts = ['0', '1', '2', '2c', '3', '3c', '4', '4c', '5', '5c', 'fuzzy'];
